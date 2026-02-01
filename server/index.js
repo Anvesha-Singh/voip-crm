8dd5afc5e7c591191b55679f818f74b435aa2125
Anvesha-Singh <anvesha.singh2022@vitstudent.ac.in>
2026-02-01 15:07:58 +0530

    Adding correct url

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const path = require('path');
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://voip-crm.vercel.app"]
+  origin: ["http://localhost:5173", "https://voip-crm-alpha.vercel.app"]
 }));
 app.use(express.json());
 
